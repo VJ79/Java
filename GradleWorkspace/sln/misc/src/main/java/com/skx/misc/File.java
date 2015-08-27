ab40fe14b1acfe0929af4ae585f8fc16c6062f49
vj79 <wjkd2222@sina.com>
2015-08-27 10:31:31 +0800

    add some configurations

---
+++ GradleWorkspace/sln/misc/src/main/java/com/skx/misc/File.java
@@ -0,0 +1,21 @@
+package com.skx.misc;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.mongodb.MongoClient;
+
+/**
+ * Created by vj on 7/2/2015.
+ */
+public class File implements IFile {
+    private static final Log LOGGER = LogFactory.getLog(File.class);
+    private MongoClient mongoClient;
+
+    public String getContent(String fileName){
+        return fileName+"ssss";
+    }
+    public File() {
+    }
+}
+
